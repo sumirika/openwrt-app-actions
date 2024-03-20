0dfd6ccf4b71b6f3979b892b118a38b10cdd1c21
ling <1042585959@qq.com>
2024-03-20 20:40:38 +0800

    初始化仓库

--- applications/luci-app-feishuvpn/luasrc/controller/feishuvpn.lua
+++
@@ -1,7 +0,0 @@
-
-module("luci.controller.feishuvpn", package.seeall)
-
-function index()
-  entry({"admin", "services", "feishuvpn"}, alias("admin", "services", "feishuvpn", "config"), _("FeiShuVpn"), 30).dependent = true
-  entry({"admin", "services", "feishuvpn", "config"}, cbi("feishuvpn"))
-end
